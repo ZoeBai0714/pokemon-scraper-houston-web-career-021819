af081605334e2e42bf0877e75c5338c81f2d008d
Zoe Bai <s.xue.bai@gmail.com>
2019-02-26 02:03:05 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,6 +12,6 @@ class Pokemon
   def self.find(id, db)
     command = "SELECT * FROM pokemon WHERE id = #{id}"
     object = db.execute(command)
-    Pokemon.new(object[0][0], object[01][1], object[0][2], db)
+    #Pokemon.new(object[0][0], object[01][1], object[0][2], db)
   end
 end
